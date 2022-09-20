6b2ff936afa64ce429594fd7b76292d19ac28e9a
BellowingButcher <mason.williams@student.awesomeinc.org>
2022-09-20 17:01:25 +0000

    fills out state of oven objects

---
+++ statePractice.js
@@ -0,0 +1,35 @@
+//pseudocode for oven
+/*
+-Door(open/closed):boolean
+
+-Preheated(true/false):boolean
+
+-convection heater:
+    -on or off
+
+-conventional heater
+    -on or off
+
+-burners
+    -frontRight
+        -
+    -frontLeft
+    -backRight
+    -backLeft
+
+-burner Dials
+    -frontRight
+        -int
+    -frontLeft
+        -int
+    -backRight
+        -int
+    -backLeft
+        -int
+
+-display-object
+        -clock
+            -on or off
+        -timer
+            -counting down or not
+*/
